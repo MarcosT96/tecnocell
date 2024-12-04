e402721f0a24d116d5ea02d5ec0f26e8fdbc1d6a
Marcos Tomassi <marcostomassi@outlook.com>
2024-12-04 12:31:09 -0300

    feat: Add name as a Title

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -2,6 +2,9 @@ import type { CollectionConfig } from 'payload'
 
 export const Categories: CollectionConfig = {
   slug: 'categories',
+  admin: {
+    useAsTitle: 'name',
+  },
   labels: {
     singular: {
       es: 'Categoría'
